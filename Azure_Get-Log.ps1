92afa46f1d6ac6e09aca468754984e8ba398330c
Diego Aquino <79656621+dinoaqui@users.noreply.github.com>
2022-03-07 17:01:03 -0300

    Create Azure_Get-Log.ps1

---
+++ Azure_Get-Log.ps1
@@ -0,0 +1,18 @@
+# Create by Thiago Oliveira
+# Get Ativity Log by date amnd genarate file logs.csv
+
+$logs = Get-AzLog -StartTime 2022-01-01 -EndTime 2022-01-30 -ResourceProvider Microsoft.KeyVault
+
+$output = foreach ($log in $logs){
+    New-Object -TypeName PSObject -Property @{
+        Authorization = $log.Authorization
+        Propiedades = $log.Properties
+        OperationName = $log.OperationName
+        User = $log.Caller
+        Data = $log.EventTimestamp
+        Level = $log.Level
+        Status = $log.Status
+    } | Select-Object Authorization, Propiedades, OperationName, User, EventTimestamp, Level, Status
+}
+
+$output | Export-Csv .\logs.csv -NoTypeInformation
